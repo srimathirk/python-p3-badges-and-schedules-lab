c9787ab87ae45a1a03253ab570a126aebcfb3fa9
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-07-13 14:10:18 -0400

    function using dS lab done

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,27 @@
 def badge_maker(name):
-    return None
+    print(f"Hello, my name is {name}.")
+    return (f"Hello, my name is {name}.")
 
 def batch_badge_creator(names):
-    return None
+    sentences=[]
+    for name in names:
+        sentence = (f"Hello, my name is {name}.")
+        sentences.append(sentence)
+    return sentences
 
 def assign_rooms(names):
-    return None
+    sentences=[]
+    for index, name in enumerate(names):
+        sentence = (f"Hello, {name}! You'll be assigned to room {index +1}!")
+        sentences.append(sentence)
+    return sentences
 
 def printer(names):
-    return None
+    sentences1 = batch_badge_creator(names)
+    sentences2 = assign_rooms(names)
+    
+    for sentence in sentences1:
+        print(sentence)
+
+    for sentence in sentences2:
+        print(sentence)
